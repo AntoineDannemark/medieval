99e6473947006ba4c3af56d254273e541f3573d7
AntoineDannemark <antoine.dannemark@protonmail.com>
2021-02-17 14:11:55 +0100

    update config with new plugins & paths + generate index.html from public folder

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,43 +1,73 @@
 const path = require('path');
 const webpack = require('webpack');
-
-const BUILD_DIR = path.resolve(__dirname, '../dist');
-const APP_DIR = path.resolve(__dirname, '../src')
+const paths = require('./paths');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = (env) => {
-    const isDev = env.NODE_ENV === 'development';
-    const isProd = env.NODE_ENV === 'production';
+    const isEnvDevelopment = env.NODE_ENV === 'development';
+    const isEnvProduction = env.NODE_ENV === 'production';
 
     return {
-        entry: APP_DIR + "/index.js",
-        mode: isProd ? "production" : isDev && "development",
+        entry: paths.appSrc + '/index.js',
+        mode: isEnvProduction ? 'production' : isEnvDevelopment && 'development',
         module: {
             rules: [
                 {
                     test: /\.(js|jsx)$/,
                     exclude: /node_modules/,
                     use: {
-                        loader: "babel-loader",
+                        loader: 'babel-loader',
                         options: { 
-                            presets: ["@babel/env"] 
+                            presets: ['@babel/env'] 
                         }
                     }
                 },
                 {
                     test: /\.css$/,
-                    use: ["style-loader", "css-loader"]
+                    use: [isEnvProduction ? MiniCssExtractPlugin.loader : 'style-loader', 'css-loader']
                 }
             ]
         },
-        resolve: { extensions: ["*", ".js", ".jsx"] },
+        plugins: [new HtmlWebpackPlugin(Object.assign(
+            {},
+            {
+              inject: true,
+              template: paths.appHtml,
+            },          
+            isEnvProduction
+                ? {
+                    minify: {
+                    removeComments: true,
+                    collapseWhitespace: true,
+                    removeRedundantAttributes: true,
+                    useShortDoctype: true,
+                    removeEmptyAttributes: true,
+                    removeStyleLinkTypeAttributes: true,
+                    keepClosingSlash: true,
+                    minifyJS: true,
+                    minifyCSS: true,
+                    minifyURLs: true,
+                    },
+                }
+                : undefined
+        ))]
+            .concat(isEnvProduction 
+                ? new MiniCssExtractPlugin({
+                    filename: 'static/css/[name].[contenthash:8].css',
+                    chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
+                }) : []),        
+        resolve: { extensions: ['*', '.js', '.jsx'] },
         output: {
-            filename: "[name]-bundle.js",
-            path: BUILD_DIR,
-            publicPath: "/",
+            filename: isEnvProduction 
+                ? 'static/js/[name].[contenthash:8].js' 
+                : isEnvDevelopment && 'static/js/bundle.js',
+            path: paths.appBuild,
+            publicPath: '/',
         },
-        devtool: isDev ? "source-map" : false,
+        devtool: isEnvDevelopment ? 'source-map' : false,
         devServer: {
-            contentBase: BUILD_DIR
+            contentBase: paths.appBuild
         }
     }
 }
